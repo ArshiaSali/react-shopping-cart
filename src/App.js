b3291dbf3c182f7e6b6e4a89ac570c6450ba04ba
ARSHIA SALI <arshiasali@gmail.com>
2020-11-05 23:44:23 -0800

    product component created

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 //feature-1
 import React from 'react';
+import Products from './components/Products';
+import data from './data.json';
 
-function App() {
-  return (
-    <div className="grid-container">
-      <header className="App-header">
-        <a href="/">Saas Shoppe</a>
-      </header>
-      <main>
-        Products
-      </main>
-      <footer>
-        All rights is reserved
-      </footer>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: ""
+    };
+  }
+  render(){
+    return (
+      <div className="grid-container">
+        <header className="App-header">
+          <a href="/">Saas Shoppe</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products products={this.state.products}> </Products>
+              </div>
+            <div className="sidebar"> Cart Items </div>
+          </div>
+        </main>
+        <footer>
+          All rights is reserved
+        </footer>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
